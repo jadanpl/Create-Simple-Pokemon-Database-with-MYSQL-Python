006ef325c5efb7da9487f7e88e7eaa9c99c0af41
Ng Pooi Ling <57357735+jadanpl@users.noreply.github.com>
2022-11-12 16:22:09 +0800

    Update dragon.sql

--- dragon.sql
+++ dragon.sql
@@ -18,7 +18,7 @@ DESCRIBE dragon;
 
 INSERT INTO dragon (`ID`, `Pokemon`, `Fast move`, `Charge move`, `DPS`, `TDO`, `Total`)
 VALUES (1, "Latos", "Dragon Breathe", "Dragon Claw", 23.91, 1243.90,17007.26),
-	   (2, "Mega Latias", "Dragon Breathe", "Outrage", 21.52, 1365.17, 13601.14),
+       (2, "Mega Latias", "Dragon Breathe", "Outrage", 21.52, 1365.17, 13601.14),
        (3, "Shadow Salamence", "Dragon Tail", "Outrage", 19.98, 836.20, 6665.55),
        (4, "Mega X Charizard", "Dragon Breathe", "Dragon Claw",19.52, 886.26, 6592.82),
        (5, "Shadow Dragonite", "Dragon Tail", "Outrage", 18.29, 864.49, 5285.85);
